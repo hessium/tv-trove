30bbaedacb8e1467f2b3c6c02b0aa962a798a6c7
hessium <hessium56@gmail.com>
2025-04-05 01:08:11 +0300

    Поправил компонент навигации

--- src/app/containers/app-header/elems/header-navigation.tsx
+++ src/app/containers/app-header/elems/header-navigation.tsx
@@ -3,8 +3,9 @@ import cl from '@/app/containers/app-header/app-header.module.scss';
 import { cn } from '@/app/shared/utils/cn';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { LINKS } from '@/app/shared/constants/links';
 import { Icon } from '@/app/shared/ui/icon/icon';
+import { AnyIconName } from '@/app/shared/ui/icon/icon';
+import { LINKS } from '@/app/shared/constants/links';
 
 export const HeaderNavigation = () => {
   const pathname = usePathname();
@@ -12,70 +13,24 @@ export const HeaderNavigation = () => {
   return (
     <nav className={cl.navigation}>
       <ul className={cl.navigation__list}>
-        <li>
-          <Link
-            className={cn(
-              cl.navigation__link,
-              pathname === LINKS.home() ? cl.navigation__link_active : '',
-            )}
-            href={LINKS.home()}
-          >
-            <Icon
-              name='common/home'
-              width='24'
-              height='24'
-            />
-            <span>Главная</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={cn(
-              cl.navigation__link,
-              pathname === LINKS.films ? cl.navigation__link_active : '',
-            )}
-            href={LINKS.films}
-          >
-            <Icon
-              name='common/movie'
-              width='24'
-              height='24'
-            />
-            <span>Фильмы</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={cn(
-              cl.navigation__link,
-              pathname === LINKS.serials ? cl.navigation__link_active : '',
-            )}
-            href={LINKS.serials}
-          >
-            <Icon
-              name='common/serial'
-              width='24'
-              height='24'
-            />
-            <span>Сериалы</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={cn(
-              cl.navigation__link,
-              pathname === LINKS.premiers ? cl.navigation__link_active : '',
-            )}
-            href={LINKS.premiers}
-          >
-            <Icon
-              name='common/tv'
-              width='24'
-              height='24'
-            />
-            <span>Премьеры</span>
-          </Link>
-        </li>
+        {LINKS.map((nav, index) => (
+          <li key={nav.pathname + index}>
+            <Link
+              className={cn(
+                cl.navigation__link,
+                pathname === nav.pathname ? cl.navigation__link_active : '',
+              )}
+              href={nav.pathname}
+            >
+              <Icon
+                name={`common/${nav.icon}` as AnyIconName}
+                width='24'
+                height='24'
+              />
+              <span>{nav.name}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
--- src/app/shared/constants/links.ts
+++ src/app/shared/constants/links.ts
@@ -1,44 +1,22 @@
-export const LINKS = {
-  auth: '/auth',
-  home: <T extends object>(params?: T) => buildUrl('/', { params }),
-  logout: '/logout',
-  serials: '/serials',
-  films: '/films',
-  premiers: '/premiers',
-  registration: '/registration',
-  profile: '/profile',
-  notAccess: '/403',
-  notFound: '/404',
-  serverError: '/500',
-};
-
-function buildUrl<T extends object>(
-  path: string,
-  { id, params }: { id?: string; params?: T },
-) {
-  if (!path) return '';
-
-  if (!id && !Object.keys(params || {}).length) {
-    return path;
-  }
-
-  if (!Object.keys(params || {}).length) {
-    return `${path}/${id}`;
-  }
-
-  const searchParams = new URLSearchParams();
-
-  for (const key of Object.keys(params || {})) {
-    const value = params?.[key as keyof BodyInit];
-
-    if (value) {
-      searchParams.append(key, value as string);
-    }
-  }
-
-  if (!id) {
-    return `${path}?${searchParams.toString()}`;
-  }
-
-  return `${path}/${id}?${searchParams.toString()}`;
-}
+export const LINKS = [
+  {
+    name: 'Главная',
+    pathname: '/',
+    icon: 'home',
+  },
+  {
+    name: 'Фильмы',
+    pathname: '/films',
+    icon: 'movie',
+  },
+  {
+    name: 'Сериалы',
+    pathname: '/serials',
+    icon: 'serial',
+  },
+  {
+    name: 'Премьеры',
+    pathname: '/premiers',
+    icon: 'tv',
+  },
+];
