cea260e8a901d72998c644bcf8dc39d90962af04
hessium <hessium56@gmail.com>
2025-04-09 00:34:43 +0300

    Убрал вертикальный скролл

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import React, { ReactNode } from 'react';
 import QueryProvider from '@/app/containers/query-client-provideer/query-client-provider';
-import '../../public/styles/app.css';
 import { BaseMeta } from '@/app/components/base-meta/base-meta';
 import { MainLayout } from '@/app/containers/main-layout/main-layout';
+
+import '../../public/styles/app.css';
 
 export default function RootLayout({
   children,
